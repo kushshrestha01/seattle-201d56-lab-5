16f6dc42619c2744baa39dd0dce729455eb286c1
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-03-29 15:00:39 -0700

    multiplication completed

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -31,11 +31,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var output = [];
+  var multiplication = a * b;
+  output.push (multiplication);
+  output.push ('The product of ' + a + ' and ' + b + ' is ' + multiplication + '.');
+  return output;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
