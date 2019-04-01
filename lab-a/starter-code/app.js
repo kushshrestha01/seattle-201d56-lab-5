6eb8f36f0d8dd0816103a9842810e59dba7b5dde
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-03-31 21:15:54 -0700

    multiplyarray() passes

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -84,7 +84,6 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
-// Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
